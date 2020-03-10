89b91f34884ff88ed706f7718e09e5923deae47f
Ryan Ofori <ryanofori1@gmail.com>
2020-03-10 06:48:17 -0400

    if not enough characters will pop up an alert for creating a question

--- StackoverflowStory/Question/QuestionVC.swift
+++ StackoverflowStory/Question/QuestionVC.swift
@@ -18,18 +18,14 @@ class QuestionVC: UIViewController {
         let title = "&title=" + (titleTxt.text ?? "")
         let body = "&body=" + (bodyTxt.text ?? "")
         let tags = "&tags=" + (tagsTxt.text ?? "")
-        //var sendUrl = "&title=" + title + "&body=" + body + "&tags=" + tags
         let sendUrl = title + body + tags
-        print(sendUrl)
-        postFunc(urlString: "https://api.stackexchange.com/2.2/questions/add/", param: "key=tUo34InxiBQXN3La2wI7Bw((&access_token=d4sFBk6dHwrUwLdIPXX(ZQ))&site=stackoverflow.com" + sendUrl)
-//        if titleTxt.text?.count ?? 0 <  15 {
-//            showAlert(mesageTitle: "Alert!", messageDesc: "Title must be at least 15 characters")
-//        } else if bodyTxt.text?.count ?? 0 < 30 {
-//            showAlert(mesageTitle: "Alert", messageDesc: "Body must be at least 30 characters")
-//        } else {
-//            postFunc(urlString: "https://api.stackexchange.com/2.2/questions/add/", param: "key=tUo34InxiBQXN3La2wI7Bw((&access_token=d4sFBk6dHwrUwLdIPXX(ZQ))&site=stackoverflow.com" + sendUrl)
-//        }
-        //post question
+        if titleTxt.text?.count ?? 0 <  15 {
+            showAlert(mesageTitle: "Alert!", messageDesc: "Title must be at least 15 characters")
+        } else if bodyTxt.text?.count ?? 0 < 30 {
+            showAlert(mesageTitle: "Alert", messageDesc: "Body must be at least 30 characters")
+        } else {
+            postFunc(urlString: "https://api.stackexchange.com/2.2/questions/add/", param: "key=tUo34InxiBQXN3La2wI7Bw((&access_token=d4sFBk6dHwrUwLdIPXX(ZQ))&site=stackoverflow.com" + sendUrl)
+        }
         
     }
     override func viewDidLoad() {
